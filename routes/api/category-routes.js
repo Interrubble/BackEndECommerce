1ff34042c98b847d6406fced0823d14d7290f4f9
Interrubble <delagarzachris@icloud.com>
2022-05-05 17:45:12 -0700

    updated put category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,10 +38,33 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const catData = await Category.update(req.body,{
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(catData => {
+    if (!catData) {
+      res.status(404).json({message:"This ID is not associated with any categories"});
+      return;
+    } else {
+      res.json(catData);
+    }
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+  });
 });
 
 router.delete('/:id', async (req, res) => {
